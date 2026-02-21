4348680588bd2d13d597434a68c98810ef3a8b58
Ikyrillos <kyrillosmaherfekri@gmail.com>
2026-02-21 14:37:09 +0200

    feat: Configure the initial Android manifest and define API endpoints.

--- lib/core/network/api_endpoints.dart
+++ lib/core/network/api_endpoints.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 
 class Endpoints {
-  static const baseUrl = kDebugMode
+  static const baseUrl = !kDebugMode
       ? "http://192.168.1.6:8081/api/"
       : "https://servicesqc.azurewebsites.net/api/";
   static const testBaseUrl =
