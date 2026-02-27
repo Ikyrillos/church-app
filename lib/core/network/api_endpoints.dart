753b47bbc10d0ec120e9348f7d5647b84f277522
Ikyrillos <kyrillosmaherfekri@gmail.com>
2026-02-27 19:21:54 +0200

    refactor: update API endpoint structure and adjust dependency type in pubspec.lock

--- lib/core/network/api_endpoints.dart
+++ lib/core/network/api_endpoints.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/foundation.dart';
-
 class Endpoints {
-  static const baseUrl = kDebugMode
-      ? "http://192.168.1.6:8081/api/"
-      : "https://servicesqc.azurewebsites.net/api/";
+  static const baseUrl = prodUrl;
+  static const prodUrl = "https://servicesqc.azurewebsites.net/api/";
+  static const dev = "https://servicesqc.azurewebsites.net/api/";
+
   static const testBaseUrl =
       "https://staging-services.azurewebsites.net/swagger/index.html/api/";
   static const requestLogin = "auth/login";
